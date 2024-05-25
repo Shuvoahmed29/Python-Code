a5f4ec69e119b6818eefda885da23260efd5d9e8
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2024-05-26 03:14:31 +0600

    Code

---
+++ Python Project/Area of any shape.py
@@ -0,0 +1,8 @@
+base = float(input("Enter Base : "))
+height = float(input("Enter Height : "))
+Triangle = 0.5*base*height
+print("Area of Triangle is : ",Triangle)
+
+radius = float(input("Enter Radius : "))
+circle = 3.1416*radius*radius
+print("Area of Circle is : ",circle)
