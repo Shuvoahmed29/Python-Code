0d3d9cfceda12addcece2fe2d99b174bcbc706d8
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2024-05-26 01:38:56 +0600

    Python

---
+++ Python Project/Backslash characters and comments.py
@@ -0,0 +1,11 @@
+#This is Single line Comment
+'''
+This is
+Multi line
+Comment
+'''
+print("Shuvo Ahmed")
+print("013999999989")
+print("Shuvo\nAhmed") #Newline
+print("Shuvo\tAhmed") #Tab
+print("\"Shuvo Ahmed\"") #Dobble Chalas
---
+++ Python Project/First Python Program.py
@@ -0,0 +1,3 @@
+print(10)
+print(3.746)
+print("Shuvo Ahmed")
